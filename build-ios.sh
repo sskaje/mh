50f640bd982537ca22d666f16ea25ca91fe3b6ff
Lessica <i.82@me.com>
2018-05-20 20:28:25 +0800

    Add debian package layout

--- build-ios.sh
+++ build-ios.sh
@@ -10,3 +10,6 @@ mkdir build/ios-arm64
 bash build/build-ios-arm64.sh
 
 lipo -create build/ios-armv7/mh_cli build/ios-armv7s/mh_cli build/ios-arm64/mh_cli -output build/mh_cli
+
+cp -p build/mh_cli build/layout/bin/
+dpkg-deb -b build/layout build/me.sskaje.mh_0.1-1_iphoneos-arm.deb
