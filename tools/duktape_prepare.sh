68f934babe3271490a51c0dfede8719238caa742
sskaje <sskaje@gmail.com>
2018-11-30 10:59:08 +0800

    no clean after duktape building

--- tools/duktape_prepare.sh
+++ tools/duktape_prepare.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-DEP_DIR=$(dirname $(dirname $(realpath $0)))/deps/duktape
+VERSION=2.3.0
 
+DEP_DIR=$(dirname $(dirname $(realpath $0)))/deps/duktape
 WORK_DIR=duktape-tmp-checkout
 
-VERSION=2.3.0
+mkdir $WORK_DIR;
+cd $WORK_DIR;
 
 echo "Downloading https://github.com/svaarala/duktape/releases/download/v${VERSION}/duktape-${VERSION}.tar.xz ..."
 curl --silent -L https://github.com/svaarala/duktape/releases/download/v${VERSION}/duktape-${VERSION}.tar.xz | tar x
@@ -25,6 +27,6 @@ cp -r extras $DEP_DIR
 cp -r licenses $DEP_DIR
 cp LICENSE.txt $DEP_DIR
 
-echo "Cleaning..."
-cd ..
-rm -rf duktape-${VERSION} v${VERSION}.tar.gz
+# echo "Cleaning..."
+# cd ..
+# rm -rf duktape-${VERSION} v${VERSION}.tar.gz
